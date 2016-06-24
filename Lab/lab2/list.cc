587e91cb88aa82621766ceb651cc23db4737ff1c
Courtney Kelly <ckelly19@nd.edu>
2016-06-23 19:45:30 -0700

    explained assignment operator overload function

--- Lab/lab2/list.cc
+++ Lab/lab2/list.cc
@@ -37,38 +37,37 @@ const List &List::operator=( const List &right ) {
         // Delete Associated Memory
             Node* next;
             for ( Node* temp = head; temp != NULL; temp = next ) {
-              next = temp->next;
-              delete temp;
+                next = temp->next;
+                delete temp;
             }
 
         // Get new space
-            int length = right.size();
-            Node* node_pointers[ length ];
+            int length = right.size();          // get the size of the list to be copied
+            Node* node_pointers[ length ];      // create an array of pointers to Node objects
 	    
-	    // Initialize array of pointers with correct number of Nodes
-            for (int i=0; i<length; i++) {
-              node_pointers[i] = new Node;
+            for (int i=0; i<length; i++) {      // Initialize array of pointers with correct number of Nodes
+                node_pointers[i] = new Node;
             }
 
         // Copy
-            Node* node_to_copy = right.head;
+            Node* node_to_copy = right.head;    // Get the head of the list to be copied in temporary node to loop through
 
-            head = node_pointers[0];
-            head->value = node_to_copy->value;
-            head->next = node_pointers[1];
+            head = node_pointers[0];            // Redirect head pointer to beginning of new Node pointers array
+            head->value = node_to_copy->value;  // assign the head's value from the copy node
+            head->next = node_pointers[1];      // assign the head's next pointer to next element in Node pointers array
 
-            for (int i=1; i<(length-1); i++) {
-              node_to_copy = node_to_copy->next;
-              node_pointers[i]->value = node_to_copy->value;
-              node_pointers[i]->next = node_pointers[i+1];
+            for (int i=1; i<(length-1); i++) {  // fill in the rest of Nodes with correct values and next pointers
+                node_to_copy = node_to_copy->next;
+                node_pointers[i]->value = node_to_copy->value;
+                node_pointers[i]->next = node_pointers[i+1];
             }
 		
-            node_to_copy = node_to_copy->next;
-            node_pointers[length-1]->value = node_to_copy->value;
+            node_to_copy = node_to_copy->next;                          // last element needs to be done outside of loop 
+            node_pointers[length-1]->value = node_to_copy->value;       // bc it's next pointer needs to point to NULL
+            delete node_to_copy;                // delete temporary node
+    }   
 
-    }  
-
-    return *this;
+    return *this;   // return yourself (basically)
 
 }
 
