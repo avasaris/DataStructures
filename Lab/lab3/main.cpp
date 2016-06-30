0381e2fff88802baf909747bfd9164815b516e9c
Courtney Kelly <ckelly19@nd.edu>
2016-06-30 09:59:46 -0700

    templated class with template implenemtation file

--- Lab/lab3/main.cpp
+++ Lab/lab3/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cctype>
 #include <vector>
+#include <string>
 #include "Stack.h"
 
 using namespace std;
@@ -54,6 +55,53 @@ int main( int argc, char * argv[] ) {
 		InFile.close();
 	}
 	
+	// Double Stack
+	cout << "\nStack of Type <double>" << endl;
+	Stack<double> doubleStack;
+	doubleStack.push(5.2);
+	doubleStack.push(100.33);
+	doubleStack.push(.02);
+	doubleStack.print();
+	cout << "Top: " << doubleStack.top() << endl;
+	doubleStack.pop();
+	doubleStack.print();
+	cout << "Top: " << doubleStack.top() << endl;
 
+	// Char Stack
+	cout << "\nStack of Type <char>" << endl;
+	Stack<char> charStack;
+	charStack.push('a');
+	charStack.push('b');
+	charStack.push('z');
+	charStack.print();
+	cout << "Top: " << charStack.top() << endl;
+	charStack.pop();
+	charStack.print();
+	cout << "Top: " << charStack.top() << endl;
+
+	// Short Stack 
+	cout << "\nStack of Type <short>" << endl;
+	Stack<short> shortStack;
+	shortStack.push(5);
+	shortStack.push(100);
+	shortStack.push(2);
+	shortStack.print();
+	cout << "Top: " << shortStack.top() << endl;
+	shortStack.pop();
+	shortStack.print();
+	cout << "Top: " << shortStack.top() << endl;
+
+	/* String Stack 
+	cout << "\nStack of Type <string>" << endl;
+	Stack<string> stringStack;
+	stringStack.push("hey");
+	stringStack.push("hi");
+	stringStack.push("hello");
+	stringStack.print();
+	cout << "Top: " << stringStack.top() << endl;
+	stringStack.pop();
+	stringStack.print();
+	cout << "Top: " << stringStack.top() << endl;
+	*/
 	return 0;
 }
