854058353ce04d1bcf7978c76df7165a99a68aa1
Courtney Kelly <ckelly19@nd.edu>
2016-07-07 10:09:20 -0700

    finished lab 4 part 2

--- Lab/lab4/main.cc
+++ Lab/lab4/main.cc
@@ -9,25 +9,22 @@
 #include <fstream>
 #include <cstdlib>
 #include <queue>
+#include <string>
 
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
   
-    if(argc != 2) {
-        cerr << "Usage error: expected <executable> <mode>" << endl;
+    if(argc != 1) {
+        cerr << "Usage error: expected <executable>" << endl;
         exit(1);
     }
       
     string cmd = "";
     int key = -1;
     BST t;
-    string mode = argv[1];
-    int numNodes=0;
 
-    // ofstream you will use to write to file output.dot
-    // currently an empty output.dot gets written into pwd
     ofstream ofs("output.dot");
     if(!ofs) {
         cout << "output.dot could not be opened." << endl;
@@ -39,43 +36,43 @@ int main(int argc, char* argv[]) {
         if(cmd == "insert") {
             cin >> key;
             t.insert(key);
-            numNodes++;
         }
         else if(cmd == "remove") {
             cin >> key;
             t.remove(key);
-            numNodes--;
         }
         else if(cmd == "dotty") {
-            ofs << "// Digraph \n// lab 4 \n// output.dot \n// cs014_16sum1\n//" << mode << " traversal" << endl << endl;
-            ofs << "digraph G {\n\n//nodes" << endl;
+            string mode;
+            cout << "What kind of dotty file would you like to generate? preorder, inorder, postorder, or height?" << endl;
+            cin >> mode;
             if (mode=="preorder") {
+                string symbol = ", pre=";
                 queue<int> preorderQ;
                 t.preorder( t.getRoot(), preorderQ );
-                t.print(ofs, preorderQ );
+                t.print(ofs, preorderQ, symbol, mode );
                 t.printEdges(ofs);
             }
             else if (mode == "postorder") {
+                string symbol = ", post=";
                 queue<int> postorderQ;
                 t.postorder( t.getRoot(), postorderQ );
-                t.print(ofs, postorderQ);
+                t.print(ofs, postorderQ, symbol, mode);
                 t.printEdges(ofs);
             }
             else if (mode == "inorder") {
+                string symbol = ", in=";
                 queue<int> inorderQ;
                 t.inorder( t.getRoot(), inorderQ );
-                t.print(ofs, inorderQ);
+                t.print(ofs, inorderQ, symbol, mode);
                 t.printEdges(ofs);
             }
             else if (mode == "height") {
-                queue<int> heightQ = t.height();
-                t.print(ofs, heightQ);
+                t.height(ofs);
                 t.printEdges(ofs);
             }
             else {
-                ofs << "Invalid mode selected" << endl;
+                cout << mode << ", not found, try again." << endl;
             }
-            ofs << "}" << endl;
         }
         else if(cmd == "display") t.display();
         else if(cmd == "end") break;
